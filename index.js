02bb0899cbd482b177d12dd8a86722d2cf8b7eab
Bowie <64285989+algominded@users.noreply.github.com>
2024-09-08 22:42:31 -0300

    Adicionado menu para interação com o usuário

--- index.js
+++ index.js
@@ -71,3 +71,109 @@ function editarTarefa(numeroId, novaDescricao) {
     tratarErroEditarTarefa(error);
   }
 }
+
+async function exibirMenu() {
+  let opcaoEscolhida;
+  do {
+    try {
+      console.log(`
+     
+      ╔═════════════════════════════════════╗
+      ║             TO-DO-LIST              ║
+      ║-------------------------------------║
+      ║      1. Adicionar Tarefa            ║
+      ║      2. Editar Tarefa               ║
+      ║      3. Remover Tarefa              ║
+      ║      4. Listar Tarefas              ║
+      ║      5. Obter Tarefa por ID         ║
+      ║      6. Sair                        ║
+      ║-------------------------------------║
+      ║-----Digite o número da sua opção----║
+      ╚═════════════════════════════════════╝
+      
+          `);
+
+      opcaoEscolhida = await question('Escolha uma opção: '); // Aguarda a escolha do usuário.
+
+      switch (opcaoEscolhida) { // Executa uma ação com base na opção escolhida.
+        case '1': // Adicionar tarefa.
+          try {
+            const descricaoTarefa = await question(`Digite a descrição para a tarefa (máximo ${LIMITE_DESCRICAO} caracteres): `);
+            adicionarTarefa(descricaoTarefa); // Chama a função para adicionar a tarefa.
+          } catch (error) {
+            tratarErroAdicionarTarefa(error); // Trata erros ao adicionar a tarefa.
+          }
+          break;
+          
+          
+        case '2':
+          try {
+            const idTarefaEditar = await question(`Digite o ID da tarefa a ser editada: `);
+            const tarefaEncontrada = listaTarefas.find(tarefa => tarefa.id === idTarefaEditar);
+
+            if (tarefaEncontrada) {
+              const novaDescricao = await question(`Digite a nova descrição da tarefa (máximo ${LIMITE_DESCRICAO} caracteres): `);
+              editarTarefa(idTarefaEditar, novaDescricao);
+            } else {
+              console.log(`Tarefa com ID ${idTarefaEditar} não foi encontrada.`);
+            }
+          } catch (error) {
+            tratarErroEditarTarefa(error);
+          }
+          break;
+
+        case '3': // Remover tarefa.
+          try {
+            const idTarefaRemover = await question(`Digite o ID da tarefa a ser removida: `); // Obtém o ID da tarefa a ser removida.
+            const indiceTarefa = listaTarefas.findIndex(tarefa => tarefa.id === idTarefaRemover); // Encontra o índice da tarefa no array.
+            if (indiceTarefa !== -1) { // Se a tarefa for encontrada, remove-a.
+              listaTarefas.splice(indiceTarefa, 1);
+              console.log(`Tarefa com ID ${idTarefaRemover} removida.`);
+            } else {
+              console.log(`Tarefa com ID ${idTarefaRemover} não encontrada.`);
+            }
+          } catch (error) {
+            tratarErroRemoverTarefa(error); // Trata erros ao remover a tarefa.
+          }
+          break;
+
+        case '4': // Listar tarefas.
+          try {
+            console.log(`4. Segue a lista de tarefas cadastradas: \n`);
+            listaTarefas.forEach(tarefa => { // Itera sobre o array de tarefas e exibe cada uma.
+              console.log(`ID: ${tarefa.id} - Descrição: ${tarefa.descricao}`);
+            });
+          } catch (error) {
+            tratarErroListarTarefas(error); // Trata erros ao listar tarefas.
+          }
+          break;
+
+        case '5': // Obter tarefa por ID.
+          try {
+            const idTarefaBuscar = await question(`Digite o ID da tarefa a ser obtida: `); // Obtém o ID da tarefa a ser buscada.
+            const tarefaEncontrada = listaTarefas.find(tarefa => tarefa.id === idTarefaBuscar); // Procura a tarefa no array.
+            if (tarefaEncontrada) {
+              console.log(`Tarefa encontrada: ID: ${tarefaEncontrada.id} - Descrição: ${tarefaEncontrada.descricao}\n`);
+            } else {
+              console.log(`Tarefa não encontrada.\n`);
+            }
+          } catch (error) {
+            tratarErroBuscarTarefa(error); // Trata erros ao buscar a tarefa.
+          }
+          break;
+
+        case '6': // Sair do programa.
+          console.log('Saindo...\n');
+          break;
+
+        default: // Caso a opção fornecida seja inválida.
+          console.log('Resposta incorreta, por favor insira um valor da tabela.\n');
+          break;
+      }
+    } catch (error) {
+      console.error('Erro inesperado:', error); // Exibe um erro inesperado.
+    } finally {
+      console.log('Operação concluída.'); // Exibe uma mensagem após cada operação.
+    }
+  } while (opcaoEscolhida !== '6'); // Continua exibindo o menu até que o usuário escolha a opção de sair.
+}
